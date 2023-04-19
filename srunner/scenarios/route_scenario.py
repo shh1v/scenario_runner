5b5683be804be62555c371a150be0e058900e642
Gustavo Silvera <grs5382@gmail.com>
2023-04-19 10:23:47 -0400

    adding minimal patches for DReyeVR+Signs support

--- srunner/scenarios/route_scenario.py
+++ srunner/scenarios/route_scenario.py
@@ -48,6 +48,9 @@ from srunner.scenariomanager.scenarioatomics.atomic_criteria import (CollisionTe
                                                                      RunningStopTest,
                                                                      ActorSpeedAboveThresholdTest)
 
+# DReyeVR utils
+from dreyevr.utils import find_ego_vehicle
+
 SECONDS_GIVEN_PER_METERS = 0.4
 
 NUMBER_CLASS_TRANSLATION = {
@@ -153,17 +156,17 @@ class RouteScenario(BasicScenario):
 
         self._update_route(world, config, debug_mode)
 
-        ego_vehicle = self._update_ego_vehicle()
+        self.ego_vehicle = self._initialize_ego_vehicle_DReyeVR(find_ego_vehicle(world))
 
         self.list_scenarios = self._build_scenario_instances(world,
-                                                             ego_vehicle,
+                                                             self.ego_vehicle,
                                                              self.sampled_scenarios_definitions,
                                                              scenarios_per_tick=5,
                                                              timeout=self.timeout,
                                                              debug_mode=debug_mode)
 
         super(RouteScenario, self).__init__(name=config.name,
-                                            ego_vehicles=[ego_vehicle],
+                                            ego_vehicles=[self.ego_vehicle],
                                             config=config,
                                             world=world,
                                             debug_mode=False,
@@ -203,6 +206,18 @@ class RouteScenario(BasicScenario):
         if debug_mode:
             self._draw_waypoints(world, self.route, vertical_shift=1.0, persistency=50000.0)
 
+    def _initialize_ego_vehicle_DReyeVR(self, ego_vehicle):
+        """
+        Set/Update the start position of the ego_vehicle (instead of _update_ego_vehicle below)
+        """
+        # move ego to correct position
+        elevate_transform = self.route[0][0]
+        elevate_transform.location.z += 0.5
+        ego_vehicle.set_transform(elevate_transform)
+        CarlaDataProvider.register_actor(ego_vehicle)
+        CarlaDataProvider.ego_DReyeVR = ego_vehicle.type_id
+        return ego_vehicle
+
     def _update_ego_vehicle(self):
         """
         Set/Update the start position of the ego_vehicle
@@ -211,7 +226,7 @@ class RouteScenario(BasicScenario):
         elevate_transform = self.route[0][0]
         elevate_transform.location.z += 0.5
 
-        ego_vehicle = CarlaDataProvider.request_new_actor('vehicle.lincoln.mkz_2017',
+        ego_vehicle = CarlaDataProvider.request_new_actor('vehicle.lincoln.mkz2017',
                                                           elevate_transform,
                                                           rolename='hero')
 
@@ -332,7 +347,7 @@ class RouteScenario(BasicScenario):
             scenario_configuration.other_actors = list_of_actor_conf_instances
             scenario_configuration.trigger_points = [egoactor_trigger_position]
             scenario_configuration.subtype = definition['scenario_type']
-            scenario_configuration.ego_vehicles = [ActorConfigurationData('vehicle.lincoln.mkz_2017',
+            scenario_configuration.ego_vehicles = [ActorConfigurationData(ego_vehicle.type_id,
                                                                           ego_vehicle.get_transform(),
                                                                           'hero')]
             route_var_name = "ScenarioRouteNumber{}".format(scenario_number)
