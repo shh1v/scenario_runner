8cc8954fb27718fd530e5ad38aa8cfb0a8a1326f
shh1v <shivpatelhw@gmail.com>
2023-07-28 15:42:14 -0700

    Added basic scenario class

---
+++ srunner/scenarios/traffic_complexity.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2023 Okanagan Visualization & Interaction (OVI) Lab
+# The University of British Columbia, BC, Canada
+#
+# This work is licensed under the terms of the MIT license.
+# For a copy, see <https://opensource.org/licenses/MIT>.
+
+"""Script used to run TOR scenario during the trial."""
+
+import random
+import py_trees
+import carla
+
+from srunner.scenariomanager.carla_data_provider import CarlaDataProvider
+from srunner.scenariomanager.scenarioatomics.atomic_behaviors import (ActorTransformSetter,
+                                                                      StopVehicle,
+                                                                      LaneChange,
+                                                                      WaypointFollower,
+                                                                      Idle)
+from srunner.scenariomanager.scenarioatomics.atomic_criteria import CollisionTest
+from srunner.scenariomanager.scenarioatomics.atomic_trigger_conditions import InTriggerDistanceToVehicle, StandStill
+from srunner.scenarios.basic_scenario import BasicScenario
+from srunner.tools.scenario_helper import get_waypoint_in_distance
+
+class TrafficComplexity(BasicScenario):
+    """
+    Some documentation on NewScenario
+    :param world is the CARLA world
+    :param ego_vehicles is a list of ego vehicles for this scenario
+    :param config is the scenario configuration (ScenarioConfiguration)
+    :param randomize can be used to select parameters randomly (optional, default=False)
+    :param debug_mode can be used to provide more comprehensive console output (optional, default=False)
+    :param criteria_enable can be used to disable/enable scenario evaluation based on test criteria (optional, default=True)
+    :param timeout is the overall scenario timeout (optional, default=60 seconds)
+    """
+
+    # some ego vehicle parameters
+    # some parameters for the other vehicles
+
+    def __init__(self, world, ego_vehicles, config, randomize=False, debug_mode=False, criteria_enable=True,
+                 timeout=60):
+        """
+        Initialize all parameters required for traffic complexity scenario
+        """
+
+        # Call constructor of BasicScenario
+        super(TrafficComplexity, self).__init__(
+          "TrafficComplexity",
+          ego_vehicles,
+          config,
+          world,
+          debug_mode,
+          criteria_enable=criteria_enable)
+
+
+    def _create_behavior(self):
+        """
+        Setup the behavior for traffic complexity scenario
+        """
+
+    def _create_test_criteria(self):
+        """
+        Setup the evaluation criteria for traffic complexity scenario
+        """
