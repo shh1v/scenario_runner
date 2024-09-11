f4b68361b24dc1c91bb7230780e52fa082304b51
luniick <karenalessandraluna@gmail.com>
2024-09-11 13:49:29 +0200

    .

--- routegenerator/route_writer.py
+++ routegenerator/route_writer.py
@@ -12,8 +12,8 @@ import re
 from lxml.etree import Element, SubElement, tostring, ElementTree
 
 # Change the parameters appropriately for your use case
-input_file = 'routegenerator/route_data/raw_waypoints/study_route_1_town05.txt'
-output_file = 'routegenerator/route_data/study_route_1_town05.xml'
+input_file = 'route_data/raw_waypoints/study_route_2_town05.txt'
+output_file = 'route_data/study_route_2_town05.xml'
 route_id = 1
 town = "Town05"
 
